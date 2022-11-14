626a01cca40461a2b3f104ce5971410869a9be16
PatinhoDaShiro <81689830+PatinhoDaShiro@users.noreply.github.com>
2022-11-14 15:36:38 -0300

    Delete TGE directory

--- TGE/login/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("DIR_PATH", "C:\\xampp/htdocs/TGE");
-define("URL_BASE","http://localhost/TGE");
-define("TEMPLATE_BASE",DIR_PATH."/templates");
-define("URL_TEMPLATE",URL_BASE."/templates");
-?>
